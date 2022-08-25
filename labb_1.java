94fc4a47d2d28524a5839fcb37b311705adc0e69
Ammar Amin <ammar.amin1975@gmail.com>
2022-08-25 09:58:46 +0200

    Create labb_1.java
    labb 1 kod

---
+++ labb_1.java
@@ -0,0 +1,21 @@
+package labb_1;
+
+public class labb_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for ( int i = 1 ; i <= 100 ; i++) {
+			if (i % 3 == 0 && i % 5 != 0) {
+				System.out.println("Fizz");
+			} else if (i % 3 != 0 && i % 5 == 0) {
+				System.out.println("Buzz");
+			} else if (i % 3 == 0 && i % 5 == 0) {
+				System.out.println("Fizzbuzz");
+			} else {
+				System.out.println(i);
+			}
+		}
+
+	}
+
+}

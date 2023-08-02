b3cde178f29ca598941bf1f2a976d175916603ff
Ammar Ali <86957756+aamin75@users.noreply.github.com>
2023-08-02 16:41:17 +0200

    change the orders of if-statements

--- labb_1.java
+++ labb_1.java
@@ -5,14 +5,14 @@ public class labb_1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		for ( int i = 1 ; i <= 100 ; i++) {
-			if (i % 3 == 0 && i % 5 != 0) {
+			if (i == 42) {
+				System.out.println("Answer to the Ultimate Question of Life, the Universe, and Everything");
+			} else if (i % 3 == 0 && i % 5 != 0) {
 				System.out.println("Fizz");
 			} else if (i % 3 != 0 && i % 5 == 0) {
 				System.out.println("Buzz");
 			} else if (i % 3 == 0 && i % 5 == 0) {
 				System.out.println("Fizzbuzz");
-			} else if (i  == 42) {
-				System.out.println("Answer to the Ultimate Question of Life, the Universe, and Everything");
 			} else {
 				System.out.println(i);
 			}
